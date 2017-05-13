41ac049ab8800b91ec9f969b02b0f5ba3c9dc6e4
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-05-13 16:41:19 +0200

    Enable WebApi

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace DistributedLoggingTracing.WebApi
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace DistributedLoggingTracing.WebApi
 {
     public static class Program
     {
         public static void Main()
         {
+            const string baseAddress = "http://localhost:9000";
+            WebApp.Start<Startup>(baseAddress);
+            Console.ReadLine();
         }
     }
 }
---
+++ src/WebApi/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace DistributedLoggingTracing.WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
+        }
+    }
+}
