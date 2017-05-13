4bbcd5867159afcf52fcb74f70637330537b6652
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-05-13 18:02:47 +0200

    Enable console logging

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using Microsoft.Owin.Hosting;
+using NLog;
 
 namespace DistributedLoggingTracing.WebApi
 {
     public static class Program
     {
+        private const string BaseAddress = "http://localhost:9000";
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public static void Main()
         {
-            const string baseAddress = "http://localhost:9000";
-            WebApp.Start<Startup>(baseAddress);
+            Logger.Debug("Starting application");
+            WebApp.Start<Startup>(BaseAddress);
             Console.ReadLine();
         }
     }
--- src/WebApi/ResourceAController.cs
+++ src/WebApi/ResourceAController.cs
@@ -1,14 +1,19 @@
 ﻿using System.Web.Http;
+using NLog;
 
 namespace DistributedLoggingTracing.WebApi
 {
     [RoutePrefix("resourceA")]
     public class ResourceAController : ApiController
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         [Route("")]
         [HttpGet]
         public IHttpActionResult Get()
         {
+            Logger.Debug($"Called {nameof(ResourceAController)}");
+
             return Ok(nameof(ResourceAController));
         }
     }
