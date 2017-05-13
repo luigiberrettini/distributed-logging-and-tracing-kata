2d6502052af5dffbc7ad04553f668d010d15e94e
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-05-13 16:42:25 +0200

    Add resource A

---
+++ src/WebApi/ResourceAController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace DistributedLoggingTracing.WebApi
+{
+    [RoutePrefix("resourceA")]
+    public class ResourceAController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(nameof(ResourceAController));
+        }
+    }
+}
