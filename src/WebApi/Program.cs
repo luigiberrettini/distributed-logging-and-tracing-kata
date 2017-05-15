86badcc7fbe6be5926f1517348cc8c259a90b651
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-05-15 14:14:35 +0200

    Remove unused logger

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using Microsoft.Owin.Hosting;
-using NLog;
 
 namespace DistributedLoggingTracing.WebApi
 {
     public static class Program
     {
         private const string BaseAddress = "http://localhost:9000";
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         public static void Main()
         {
